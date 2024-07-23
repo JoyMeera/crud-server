5d42825cd14db003b273d8f70541af80f3514e8b
reebujoy <reebsmj@gmail.com>
2024-07-23 09:52:11 +0100

    Added environment variables

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Initalisation of server
+require('dotenv').config()
 const app = require('./app')
-const port = 3000
+const port = process.env.PORT || 5000 //the 5000 is a backup is incase port 3000 is busy
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
